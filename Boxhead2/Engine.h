4b8e5820cc0c97bf29104d414e90e5e5e03e4621
Wojtek <wojtekgg93@gmail.com>
2023-10-25 23:26:04 +0200

    add: Engine files

---
+++ Boxhead2/Engine.h
@@ -0,0 +1,6 @@
+#ifndef Engine_h
+#define Engine_h
+
+void init();
+
+#endif
--- Boxhead2/main.cpp
+++ Boxhead2/main.cpp
@@ -1,24 +1,4 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_native_dialog.h>
-
-#define screenWidth 2500
-#define screenHeight 1350
-
-
-void init() {
-	ALLEGRO_DISPLAY* display = NULL;
-	al_init();
-	display = al_create_display(screenWidth, screenHeight);
-
-	while (true) {
-
-	}
-
-
-}
+#include "Engine.h"
 
 using namespace std;
 
