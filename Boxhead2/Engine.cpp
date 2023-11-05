db4cc785aefe2d4e60ed02650f9d9d6c0e281c64
Wojtek <wojtekgg93@gmail.com>
2023-11-05 17:59:15 +0100

    add: exit by x

--- Boxhead2/Engine.cpp
+++ Boxhead2/Engine.cpp
@@ -1,12 +1,13 @@
 #include <allegro5/allegro.h>
-//#include <allegro5/allegro_primitives.h>
-//#include <allegro5/allegro_font.h>
-//#include <allegro5/allegro_ttf.h>
-//#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_native_dialog.h>
 
 #include "Engine.h"
 #include "Control.h"
 #include "Interface.h"
+#include <iostream>
 
 #define screenWidth 1800
 #define screenHeight 900
@@ -18,6 +19,11 @@ void init() {
 	// initiation 
 	ALLEGRO_DISPLAY* display = NULL;
 	al_init();
+	al_install_keyboard();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_init_primitives_addon();
+
 
 
 	/*
@@ -28,10 +34,30 @@ void init() {
 	al_set_window_position(display, SCREEN_POSITION_X, SCREEN_POSITION_Y);
 	al_set_window_title(display, "Boxhead2");
 
+	ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+
+	if (!event_queue) {
+		std::cerr << "B³¹d inicjalizacji okna." << std::endl;
+		return;
+	}
+		
+
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	/*
 		 MAIN LOOP
 	*/
+
+	al_flip_display();
 	while (true) {
+		ALLEGRO_EVENT event;
+		al_wait_for_event(event_queue, &event);
+		
+
+		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
 
 	}
 
@@ -39,4 +65,3 @@ void init() {
 
 	al_destroy_display(display);
 }
-
