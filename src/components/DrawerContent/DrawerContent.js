eeac7babd3159a39165beeb536d335402a2f8fd9
Yehor <floatice666@gmail.com>
2023-12-16 17:21:25 +0100

    add: ekran achievement

--- src/components/DrawerContent/DrawerContent.js
+++ src/components/DrawerContent/DrawerContent.js
@@ -16,8 +16,8 @@ const CustomDrawer = ({navigateToScreen}) =>{
         <View style={styles.drawerWrapper}>
             <DrawerListItem title='Strona Główna' link='Home' handler={navigateToScreen} icon={<Entypo name="home" size={24} color={colors.primary} />}/>
             <DrawerListItem title='Plan treningowy' link='Training Plan' handler={navigateToScreen} icon={<Ionicons name="md-calendar-sharp" size={24} color={colors.primary} />}/>
-            <DrawerListItem title='Statystyki' link='Training Plan Creator' handler={navigateToScreen} icon={<Fontisto name="line-chart" size={24}  color={colors.primary}  />}/>
-            <DrawerListItem title='Osiągnięcia' link='Training Plan Creator' handler={navigateToScreen} icon={<MaterialCommunityIcons name="star" size={28} color={colors.primary}/>}/>
+            <DrawerListItem title='Statystyki' link='Statistic' handler={navigateToScreen} icon={<Fontisto name="line-chart" size={24}  color={colors.primary}  />}/>
+            <DrawerListItem title='Osiągnięcia' link='Achievement' handler={navigateToScreen} icon={<MaterialCommunityIcons name="star" size={28} color={colors.primary}/>}/>
             <DrawerListItem title='Kreator planu' link='Training Plan Creator' handler={navigateToScreen} icon={<SimpleLineIcons name="note" size={24} color={colors.primary} />}/>
             <DrawerListItem title='Atlas ćwiczeń' link='Training Plan Creator' handler={navigateToScreen} icon={<FontAwesome name="folder" size={24}  color={colors.primary} />}/>
             <DrawerListItem title='Zapisz postęp' link='Training Plan Creator' handler={navigateToScreen} icon={<MaterialIcons name="add-a-photo" size={24} color={colors.primary} />}/>
---
+++ src/screens/Statistic/Statistic.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native-paper"
+import { View } from "react-native"
+import styles from "@utils/styles"
+
+
+const Statictic = () => {
+    return (
+        <View style={styles.containerNoCenter}>
+            <Text style={{color: 'white'}}>Statictic</Text>
+        </View>
+    )
+}
+export default Statictic
