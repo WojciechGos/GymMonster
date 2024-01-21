57049c824514d2111846cec52f6a125f872d9b51
Wojtek <wojtekgg93@gmail.com>
2024-01-21 22:39:12 +0100

    minor

--- src/components/StatisticAccordion/StatisticAccordionContainer.js
+++ src/components/StatisticAccordion/StatisticAccordionContainer.js
@@ -1,6 +1,6 @@
 import StatisticAccordion from "./StatisticAccordion"
 
-import excercisesHistoryListData from "@data/excercisesHistoryListData" 
+import excercisesHistoryListData from "@data/excercisesHistoryListData"
 
 import { FIRESTORE_DB } from "../../../firebaseConfig"
 import AsyncStorage from "@react-native-async-storage/async-storage"
@@ -12,24 +12,48 @@ import {
     getDocs,
 } from "firebase/firestore"
 
-
-
-const StatisticAccordionContainer = ({route}) => {
+const StatisticAccordionContainer = ({ route }) => {
     const item = route.params?.item
 
     console.log(item)
-    
+
     const [history, setHistory] = useState([])
-    
-    useEffect(()=>{
-        
+
+    useEffect(() => {
+        const fetchTrainingHistory = async (userId, exerciseName) => {
+            const trainingHistoryCollection = collection(
+                FIRESTORE_DB,
+                "trainingHistoric"
+            )
+
+            // Create a query to get documents where userId matches and name is equal to item.name
+            const q = query(
+                trainingHistoryCollection,
+                where("userId", "==", userId),
+                where("name", "==", exerciseName),
+                orderBy("timestamp", "desc") // You can adjust the ordering based on your needs
+            )
+
+            try {
+                const querySnapshot = await getDocs(q)
+                const historyData = []
+
+                querySnapshot.forEach((doc) => {
+                    // Assuming each document has a data() method to get its data
+                    const data = doc.data()
+                    historyData.push(data)
+                })
+
+                return historyData
+            } catch (error) {
+                console.error("Error fetching training history:", error)
+                return []
+            }
+        }
+
         // console.log(item)
         console.log("StatisticAccordionContainer item", item)
-
-        
-        
-
-    },[item])
+    }, [item])
 
     const props = {
         data: excercisesHistoryListData,
